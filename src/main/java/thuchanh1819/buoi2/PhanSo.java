31b8728fd59962591911154639eb05619fe9281c
ngthtrong <ngthtrong204@gmail.com>
2024-12-28 10:18:45 +0700

    done buoi 2 18-19

--- src/main/java/thuchanh1819/buoi2/PhanSo.java
+++ src/main/java/thuchanh1819/buoi2/PhanSo.java
@@ -6,6 +6,18 @@ public class PhanSo {
 	private int TuSo;
 	private int MauSo;
 
+	private void chuanHoa() {
+		int a = this.TuSo;
+		int b = this.MauSo;
+		while (a % b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		this.TuSo /= b;
+		this.MauSo /= b;
+	}
+
 	public int getTuSo() {
 		return TuSo;
 	}
@@ -34,17 +46,18 @@ public class PhanSo {
 
 	public void nhapPhanSo() {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Nhap tu so:");
-		this.TuSo = scan.nextInt();
+		System.out.print("Nhap tu so: ");
+		if (scan.hasNextInt())
+			this.TuSo = scan.nextInt();
 		this.MauSo = 0;
 		while (this.MauSo == 0) {
 			System.out.print("Nhap mau so: ");
 			this.MauSo = scan.nextInt();
-			if (this.MauSo == 0) {
+			if (this.MauSo == 0)
 				System.out.println("Vui long nhap mau so khac 0!");
-			}
+
 		}
-		scan.close();
+		this.chuanHoa();
 	}
 
 	public void hienThiPhanSo() {
@@ -53,8 +66,80 @@ public class PhanSo {
 			return;
 		}
 		System.out.println(this.TuSo + "/" + this.MauSo);
-		System.out.println(this.TuSo + "/" + this.MauSo);
+	}
 
+	public void nghichDao() {
+		if (this.TuSo == 0) {
+			System.out.println("Khong the nghich dao phan so 0");
+		} else {
+			int temp = this.TuSo;
+			this.TuSo = this.MauSo;
+			this.MauSo = temp;
+		}
+	}
+
+	public PhanSo giaTriNghichDao() {
+		if (this.TuSo == 0) {
+			System.out.println("Khong the nghich dao phan so 0");
+			return this;
+		} else
+			return new PhanSo(this.MauSo, this.TuSo);
+	}
+
+	public double giaTriPhanSo() {
+		return (double) this.TuSo / this.MauSo;
+	}
+
+	public boolean lonHon(PhanSo a) {
+		return this.giaTriPhanSo() > a.giaTriPhanSo();
+	}
+
+	public PhanSo cong(PhanSo a) {
+		if (this.TuSo == 0)
+			return a;
+		if (a.TuSo == 0)
+			return this;
+		PhanSo rs = new PhanSo(this.TuSo * a.MauSo + a.TuSo * this.MauSo, this.MauSo * a.MauSo);
+		rs.chuanHoa();
+		return rs;
+	}
+
+	public PhanSo tru(PhanSo a) {
+		if (this.TuSo == 0) {
+			a.setTuSo(-a.getTuSo());
+			return a;
+		}
+		if (a.TuSo == 0)
+			return this;
+		PhanSo rs = new PhanSo(this.TuSo * a.MauSo - a.TuSo * this.MauSo, this.MauSo * a.MauSo);
+		rs.chuanHoa();
+		return rs;
+	}
+
+	public PhanSo nhan(PhanSo a) {
+		PhanSo rs = new PhanSo(this.TuSo * a.TuSo, this.MauSo * a.MauSo);
+		rs.chuanHoa();
+		return rs;
+	}
+
+	public PhanSo chia(PhanSo a) {
+		return nhan(a.giaTriNghichDao());
+	}
+
+	public PhanSo cong(int n) {
+		return this.cong(new PhanSo(n, 1));
+	}
+
+	public PhanSo tru(int n) {
+		return this.tru(new PhanSo(n, 1));
+	}
+
+	public PhanSo nhan(int n) {
+		return this.nhan(new PhanSo(n, 1));
+	}
+
+	public PhanSo chia(int n) {
+		return this.chia(new PhanSo(n, 1));
 	}
 
 }
---
+++ src/main/java/thuchanh1819/buoi2/UsePhanSo.java
@@ -0,0 +1,60 @@
+package thuchanh1819.buoi2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UsePhanSo {
+	public static void main(String[] args) {
+		PhanSo a = new PhanSo(3, 7);
+		PhanSo b = new PhanSo(4, 9);
+		a.hienThiPhanSo();
+		b.hienThiPhanSo();
+
+		PhanSo x = new PhanSo();
+		PhanSo y = new PhanSo();
+		x.nhapPhanSo();
+		y.nhapPhanSo();
+		x.hienThiPhanSo();
+		y.hienThiPhanSo();
+		
+		
+		
+		x.giaTriNghichDao().hienThiPhanSo();
+
+		x.cong(y).hienThiPhanSo();
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Nhap n: ");
+		int n = scan.nextInt();
+
+		List<PhanSo> list = new ArrayList<>();
+
+		PhanSo total = new PhanSo(0, 1);
+		PhanSo max = new PhanSo(-1000, 1);
+
+		for (int i = 0; i < n; i++) {
+			PhanSo temp = new PhanSo();
+			temp.nhapPhanSo();
+			list.add(temp);
+			total = total.cong(temp);
+			if (!max.lonHon(temp))
+				max = temp;
+		}
+		scan.close();
+		System.out.print("Tong n phan so: ");
+		total.hienThiPhanSo();
+
+		System.out.print("Max in n phan so: ");
+		max.hienThiPhanSo();
+
+		System.out.println("Danh sach phan so:");
+		list.sort((p1, p2) -> {
+			return !p1.lonHon(p2) ? -1 : 1;
+		});
+		for (PhanSo p : list) {
+			p.hienThiPhanSo();
+		}
+
+	}
+}
